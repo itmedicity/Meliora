c43be07e81b9d5826ac49d65176b1483fc438edc
Ajith Arjun <ajithajeesh@gmail.com>
2022-06-13 18:18:05 +0530

    New Theme setup

---
+++ src/views/Components/CardOne.jsx
@@ -0,0 +1,32 @@
+import { Card, CardActions, CardContent } from '@mui/material';
+import React, { Fragment } from 'react';
+import { ToastContainer } from 'react-toastify';
+import ValidateSession from '../Axios/ValidateSession';
+import CustomCardHeader from './CustomCardHeader';
+import SaveButton from './SaveButton';
+import CancelButton from './CancelButton';
+import EditButton from './EditButton';
+import RefreshButton from './RefreshButton';
+
+const CardOne = (props) => {
+    return (
+        <Fragment>
+            <ToastContainer />
+            <ValidateSession />
+            <Card>
+                <CustomCardHeader title={"Card One"} />
+                <CardContent>
+                    {props.children}
+                </CardContent>
+                <CardActions disableSpacing sx={{ paddingY: 0 }}>
+                    <SaveButton />
+                    <EditButton />
+                    <RefreshButton />
+                    <CancelButton />
+                </CardActions>
+            </Card>
+        </Fragment>
+    )
+}
+
+export default CardOne
---
+++ src/views/Components/EditButton.jsx
@@ -0,0 +1,19 @@
+import { IconButton, Tooltip } from '@mui/material'
+import React from 'react'
+import EditOffIcon from '@mui/icons-material/EditOff';
+
+const EditButton = () => {
+    return (
+        <Tooltip title="Edit" arrow >
+            <IconButton
+                aria-label="add to favorites"
+                disableRipple={true}
+                sx={{ color: "#7ac143", paddingY: 0.5, }}
+            >
+                <EditOffIcon sx={{ fontSize: 25 }} />
+            </IconButton>
+        </Tooltip>
+    )
+}
+
+export default EditButton
