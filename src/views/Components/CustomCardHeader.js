cd7298a73a30881c06351b12e4ac4fccea4e2bd7
Ajith Arjunan <ajithajeesh@gmail.com>
2022-06-22 08:13:11 +0530

    Material Table Issue Resolved

--- src/views/Components/CustomCardHeader.js
+++ src/views/Components/CustomCardHeader.js
@@ -1,32 +1,45 @@
 import React from 'react'
-import { CardHeader, IconButton, Tooltip } from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
-import { cardHeaderColor, cardHeaderFont } from 'src/color/Color';
+import { CardHeader, IconButton, Tooltip } from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close'
+import { cardHeaderColor, cardHeaderFont } from 'src/color/Color'
 
 const CustomCardHeader = ({ title }) => {
-    return (
-        <CardHeader
-            title={title}
-            action={
-                <Tooltip title="Close" arrow >
-                    <IconButton aria-label="settings" size="small" sx={{ paddingTop: 2 }} >
-                        <CloseIcon sx={{ paddingTop: 0.3, color: "#fdfdfd" }} />
-                    </IconButton>
-                </Tooltip>
-            }
-            titleTypographyProps={{ variant: "subtitle2" }}
-            disableTypography
-            sx={{
-                backgroundColor: cardHeaderColor,
-                paddingY: 0,
-                borderBottom: 0.1,
-                borderBottomColor: cardHeaderColor,
-                // fontWeight: "bold",
-                fontSize: 14,
-                color: cardHeaderFont
-            }}
-        />
-    )
+  return (
+    <CardHeader
+      title={title}
+      action={
+        <Tooltip title="Close" arrow>
+          <IconButton aria-label="settings" size="small" sx={{ paddingTop: 2 }}>
+            <CloseIcon sx={{ paddingTop: 0.3, color: cardHeaderFont }} />
+          </IconButton>
+        </Tooltip>
+      }
+      titleTypographyProps={{ variant: 'subtitle2' }}
+      //   disableTypography
+      sx={{
+        backgroundColor: cardHeaderColor,
+        paddingY: 0.5,
+        borderBottom: 0.1,
+        borderBottomColor: cardHeaderColor,
+        fontSize: 14,
+        color: cardHeaderFont,
+        border: 0.1,
+        borderColor: '#66bb6a',
+        fontFamily: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+        ].join(','),
+      }}
+    />
+  )
 }
 
 export default CustomCardHeader
