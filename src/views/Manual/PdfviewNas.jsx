e27b003664d23a298d9f08f4e09a23c0249b3b7d
IT Medicity <itmedicity@tmc.ac.in>
2023-05-05 15:47:50 +0530

    NABH Guide LIne Display

--- src/views/Manual/PdfviewNas.jsx
+++ src/views/Manual/PdfviewNas.jsx
@@ -1,25 +1,32 @@
 import React from 'react'
 import { Fragment, memo } from 'react';
+import { Box, Typography } from '@mui/material'
 
 const PdfviewNas = ({ pdfDis }) => {
     return (
         <Fragment>
-            {
-                pdfDis === 1 ?
-                    <embed
-                        src={"http://192.168.10.170/NAS//fileshows/GuideLine.pdf"}
-                        type="application/pdf"
-                        height={850}
-                        width={1800} />
+            <Box sx={{
+                width: { md: "100%", sm: "100%", xl: "100%", lg: "100%", xs: "100%" }
 
-                    : pdfDis === 3 ?
+            }}>
+                {
+                    pdfDis === 1 ?
                         <embed
-                            src={"http://192.168.10.170/NAS//fileshows/GuideLine.pdf"}
+                            src={"http://192.168.10.170/NAS//fileshows/GuideLine.pdf" + "#toolbar=0"}
                             type="application/pdf"
                             height={850}
-                            width={1800} />
-                        : null
-            }
+                            width="100%" />
+
+                        : pdfDis === 3 ?
+                            <embed
+                                src={"http://192.168.10.170/NAS//fileshows/GuideLine.pdf"}
+                                type="application/pdf"
+                                height={850}
+                                width="100%" />
+                            : null
+                }
+
+            </Box>
 
         </Fragment>
     )
