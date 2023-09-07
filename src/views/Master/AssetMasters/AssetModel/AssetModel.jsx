422756cf8d0ca0194fb5c64521e5f05c707545ca
unknown <Administrator@MEDICITY.CO.IN>
2023-09-07 15:08:07 +0530

    File upload and compress file in model master

--- src/views/Master/AssetMasters/AssetModel/AssetModel.jsx
+++ src/views/Master/AssetMasters/AssetModel/AssetModel.jsx
@@ -1,8 +1,5 @@
 import { Box } from '@mui/material'
-import React, { memo } from 'react'
-import { useCallback } from 'react'
-import { useMemo } from 'react'
-import { useState } from 'react'
+import React, { memo, useCallback, useMemo, useState } from 'react'
 import { axioslogin } from 'src/views/Axios/Axios'
 import { infoNotify, succesNotify } from 'src/views/Common/CommonCode'
 import CardMaster from 'src/views/Components/CardMaster'
@@ -10,11 +7,13 @@ import CusCheckBox from 'src/views/Components/CusCheckBox'
 import TextFieldCustom from 'src/views/Components/TextFieldCustom'
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
 import ModelTable from './ModelTable'
+import imageCompression from 'browser-image-compression';
 
 const AssetModel = () => {
   const [value, setValue] = useState(0)
   const [count, setCount] = useState(0)
   const history = useHistory()
+  const [selectFile, setSelectFile] = useState(null)
   const [model, setModel] = useState({
     model_slno: '',
     model_name: '',
@@ -61,24 +60,25 @@ const AssetModel = () => {
     setModel(frmdata)
     setCount(0)
     setValue(0)
+    setSelectFile(null)
   }
+
+  const uploadFile = async (event) => {
+    const file = event.target.files[0];
+    const options = {
+      maxSizeMB: 1,
+      maxWidthOrHeight: 1920
+    }
+    const compressedFile = await imageCompression(file, options);
+    setSelectFile(compressedFile);
+  };
+
   const submitModel = useCallback(
     (e) => {
       e.preventDefault()
-
       const InsertModel = async (postdata) => {
         const result = await axioslogin.post('/model/insert', postdata)
-
-        const { message, success } = result.data
-        if (success === 1) {
-          succesNotify(message)
-          setCount(count + 1)
-          reset()
-        } else if (success === 0) {
-          infoNotify(message)
-        } else {
-          infoNotify(message)
-        }
+        return result.data
       }
       const ModelUpdate = async (patchdata) => {
         const result = await axioslogin.patch('/model/update', patchdata)
@@ -94,13 +94,39 @@ const AssetModel = () => {
         }
       }
 
+      const FileInsert = async (fileData) => {
+        const result = await axioslogin.post('/fileupload/uploadFile/Model', fileData)
+        const { message, success } = result.data
+        if (success === 1) {
+          succesNotify(message)
+          setCount(count + 1)
+          reset()
+        }
+        else {
+          infoNotify(message)
+        }
+      }
       if (value === 0) {
-        InsertModel(postdata)
+        InsertModel(postdata).then((val) => {
+          const { message, success, insertid } = val
+          if (success === 1) {
+            //File upload Api and post data
+            const formData = new FormData()
+            formData.append('id', insertid)
+            formData.append('file', selectFile, selectFile.name)
+            FileInsert(formData)
+          }
+          else if (success === 0) {
+            infoNotify(message)
+          } else {
+            infoNotify(message)
+          }
+        })
       } else {
         ModelUpdate(patchdata)
       }
     },
-    [postdata, value, patchdata, count],
+    [postdata, value, patchdata, count, selectFile],
   )
 
   const backtoSetting = useCallback(() => {
@@ -115,6 +141,7 @@ const AssetModel = () => {
     setModel(frmdata)
     setValue(0)
   }, [setModel])
+
   return (
     <CardMaster title="Model" submit={submitModel} close={backtoSetting} refresh={refreshWindow}>
       <Box sx={{ height: '100%', width: '100%', display: 'flex' }}>
@@ -139,6 +166,15 @@ const AssetModel = () => {
               checked={model_status}
               onCheked={UpdateModel}
             ></CusCheckBox>
+          </Box>
+          <Box sx={{
+            width: "60%",
+            display: "flex",
+            flexDirection: "column"
+          }}>
+            <input type='file'
+              onChange={uploadFile}
+              required></input>
           </Box>
         </Box>
         <Box sx={{ width: '70%' }}>
