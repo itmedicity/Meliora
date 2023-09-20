a9a25108deaf01c9ff231bbf6aa183ff8fa2e187
unknown <Administrator@MEDICITY.CO.IN>
2023-09-20 12:27:28 +0530

    changes

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -5,7 +5,7 @@ const DefaultLayout = () => {
   return (
     <div>
       <AppSidebar />
-      <div className="wrapper flex flex-column min-vh-100 " style={{ backgroundColor: "#e0f2f1" }} >
+      <div className="wrapper flex flex-column min-vh-100 " style={{ backgroundColor: "#BEC1C5" }} >
         <AppHeader />
         <div className="body flex-grow-1 px-0 py-2" >
           <AppContent />
