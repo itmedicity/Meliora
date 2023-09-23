2d4d79e25af68d157b68f1accca9b6ac299ba29f
Ajith Arjunan <ajithajeesh@gmail.com>
2023-09-23 12:58:58 +0530

    task mamage

--- src/views/TaskManagement/Home/TaskHomePage.jsx
+++ src/views/TaskManagement/Home/TaskHomePage.jsx
@@ -3,6 +3,7 @@ import { Paper } from '@mui/material';
 import React, { useEffect, useState } from 'react'
 import { memo } from 'react'
 import { innerHeight } from 'src/views/Constant/Constant';
+import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
 
 const TaskHomePage = () => {
 
@@ -19,18 +20,33 @@ const TaskHomePage = () => {
       flexDirection: 'column',
       border: 1,
       borderWidth: 1.5,
-      borderColor: '#e0dedc'
+      borderColor: '#6D6E6F'
     }} >
+      <Box
+        sx={{
+          display: 'flex',
+          flex: 1,
+          flexDirection: 'row',
+          backgroundColor: '#6D6E6F',
+          maxHeight: 40,
+          alignItems: 'center',
+          pl: 2
+        }}
+      >
+        <DashboardOutlinedIcon fontSize='medium' sx={{ color: 'white' }} />
+        <Box sx={{ pl: 1, color: 'white', display: 'flex', pt: 0.3 }} >My Dashboard</Box>
+      </Box>
       <Box sx={{
         display: 'flex',
         flex: 1,
         flexDirection: 'row',
         flexWrap: 'wrap',
         padding: 1,
-        backgroundColor: '#e0dedc',
+        // backgroundColor: '#e0dedc',
         overflow: 'hidden'
         // border: 1
       }} >
+
         <Paper
           sx={{
             width: '20%',
