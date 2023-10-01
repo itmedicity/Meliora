ee3135000418f9f23a47a2c0873abb209c3fc287
unknown <Administrator@MEDICITY.CO.IN>
2023-10-01 19:00:42 +0530

    task setting

--- src/views/TaskManagement/Components/TaskUpcomingCmp.jsx
+++ src/views/TaskManagement/Components/TaskUpcomingCmp.jsx
@@ -1,12 +1,35 @@
-import { Box, CssVarsProvider } from '@mui/joy'
+import { Box, CssVarsProvider, Sheet } from '@mui/joy'
 import React from 'react'
 import { memo } from 'react'
 import Table from '@mui/joy/Table';
 import Button from '@mui/joy/Button';
 import AddIcon from '@mui/icons-material/Add';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import TaskTableRow from './TaskTableRow';
+import { innerHeight } from 'src/views/Constant/Constant';
 
-
-
+const task = [
+    { slno: 1, task: "Lorpam 1mg Tablet is used in the treatment of Short term anxiety,Anxiety disorder.", due: '2023-09-25' },
+    { slno: 2, task: "dasdasdasds dsfsdfsf sfsdfsd  dgdfgd  dgfdfgfdg", due: new Date() },
+    { slno: 3, task: "dasdasdasds sdfsdf sdfsdf sdfgdfg yj tyujyt ty ty", due: new Date() },
+    { slno: 4, task: "dasdasdasds trfyhrty rt tyrtytr rtyrty sfsdf sdfsdfsyuh ytutyuytu ", due: new Date() },
+    { slno: 5, task: "dasdasdasds", due: '2023-09-25' },
+    { slno: 5, task: "dasdasdasds", due: '2023-09-26' },
+    { slno: 5, task: "dasdasdasds", due: '2023-09-27' },
+    { slno: 5, task: "dasdasdasds", due: '2023-09-28' },
+    { slno: 5, task: "dasdasdasds", due: '2023-09-29' },
+    { slno: 5, task: "dasdasdasds", due: '2023-09-30' },
+    { slno: 5, task: "dasdasdasds", due: '2023-10-01' },
+    { slno: 5, task: "dasdasdasds", due: '2023-10-02' },
+    { slno: 5, task: "dasdasdasds", due: '2023-10-03' },
+    { slno: 4, task: "dasdasdasds trfyhrty rt tyrtytr rtyrty sfsdf sdfsdfsyuh ytutyuytu ", due: new Date() },
+    { slno: 4, task: "dasdasdasds trfyhrty rt tyrtytr rtyrty sfsdf sdfsdfsyuh ytutyuytu ", due: new Date() },
+    { slno: 4, task: "dasdasdasds trfyhrty rt tyrtytr rtyrty sfsdf sdfsdfsyuh ytutyuytu ", due: new Date() },
+    { slno: 4, task: "dasdasdasds trfyhrty rt tyrtytr rtyrty sfsdf sdfsdfsyuh ytutyuytu ", due: new Date() },
+    { slno: 4, task: "dasdasdasds trfyhrty rt tyrtytr rtyrty sfsdf sdfsdfsyuh ytutyuytu ", due: new Date() },
+    { slno: 4, task: "dasdasdasds trfyhrty rt tyrtytr rtyrty sfsdf sdfsdfsyuh ytutyuytu ", due: new Date() },
+    { slno: 4, task: "dasdasdasds trfyhrty rt tyrtytr rtyrty sfsdf sdfsdfsyuh ytutyuytu ", due: new Date() },
+]
 const TaskUpcomingCmp = () => {
     return (
         <CssVarsProvider>
@@ -15,59 +38,34 @@ const TaskUpcomingCmp = () => {
             }} >
                 <Button onClick={function () { }} variant="plain" startDecorator={<AddIcon />} size="sm" >Create task</Button>
             </Box>
-            <Table
-                aria-label="basic table"
-                sx={{ '& thead th:nth-of-type(1)': { width: '50%' }, borderTop: 1, borderTopColor: '#d0d2d5' }}
-                hoverRow
-                borderAxis='x'
-            >
-                {/* <thead>
-                    <tr>
-                        <th style={{ width: '40%' }}>Dessert (100g serving)</th>
-                        <th>Calories</th>
-                        <th>Fat&nbsp;(g)</th>
-                        <th>Carbs&nbsp;(g)</th>
-                        <th>Protein&nbsp;(g)</th>
-                    </tr>
-                </thead> */}
-                <tbody  >
-                    <tr>
-                        <td>Frozen yoghurt</td>
-                        <td>159</td>
-                        <td>6</td>
-                        <td>24</td>
-                        <td>4</td>
-                    </tr>
-                    <tr>
-                        <td>Ice cream sandwich</td>
-                        <td>237</td>
-                        <td>9</td>
-                        <td>37</td>
-                        <td>4.3</td>
-                    </tr>
-                    <tr>
-                        <td>Eclair</td>
-                        <td>262</td>
-                        <td>16</td>
-                        <td>24</td>
-                        <td>6</td>
-                    </tr>
-                    <tr>
-                        <td>Cupcake</td>
-                        <td>305</td>
-                        <td>3.7</td>
-                        <td>67</td>
-                        <td>4.3</td>
-                    </tr>
-                    <tr>
-                        <td>Gingerbread</td>
-                        <td>356</td>
-                        <td>16</td>
-                        <td>49</td>
-                        <td>3.9</td>
-                    </tr>
-                </tbody>
-            </Table>
+            <Sheet sx={{ height: `${innerHeight - 500}px`, overflow: 'auto' }}>
+                <Table
+                    aria-label="basic table"
+                    sx={{
+                        "--TableCell-height": "28px",
+                        "--TableCell-paddingX": "0px",
+                        "--TableCell-paddingY": "0px",
+                        "--Table-headerUnderlineThickness": "1px",
+                        borderTop: 1,
+                        borderTopColor: '#d0d2d5',
+                    }}
+                    hoverRow
+                    stickyFooter
+                    borderAxis='x'
+                    size='sm'
+                >
+                    <tbody>
+                        {
+                            task?.map((e, idx) => <TaskTableRow key={idx} taskName={e.task} dueDate={e.due} />)
+                        }
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td>dsfsd</td>
+                        </tr>
+                    </tfoot>
+                </Table>
+            </Sheet>
         </CssVarsProvider>
     )
 }
--- src/views/TaskManagement/Home/TaskHomePage.jsx
+++ src/views/TaskManagement/Home/TaskHomePage.jsx
@@ -14,80 +14,78 @@ import MyTaskTable from './MyTaskTable';
 
 const TaskHomePage = () => {
 
-  const data = [
-    { taskName: 'Completed task', count: 10, icon: <AccountTreeIcon /> },
-    { taskName: 'Incomplete task', count: 121, icon: <AlignHorizontalLeftIcon /> },
-    { taskName: 'Overdue task', count: 18, icon: <FormatOverlineIcon /> },
-    { taskName: 'Total task', count: 215, icon: <DoNotDisturbOnTotalSilenceIcon /> }
-  ]
+    const data = [
+        { taskName: 'Completed task', count: 10, icon: <AccountTreeIcon /> },
+        { taskName: 'Incomplete task', count: 121, icon: <AlignHorizontalLeftIcon /> },
+        { taskName: 'Overdue task', count: 18, icon: <FormatOverlineIcon /> },
+        { taskName: 'Total task', count: 215, icon: <DoNotDisturbOnTotalSilenceIcon /> }
+    ]
 
-  return (
-    <Box
-      sx={{
-        display: 'flex',
-        minHeight: `${innerHeight - 80}px`,
-        borderRadius: 2,
-        overflow: 'hidden',
-        flexDirection: 'column',
-        border: 1,
-        borderWidth: 1.5,
-        borderColor: taskColor.bgIndigo,
-      }} >
-      <Box
-        sx={{
-          display: 'flex',
-          flex: 1,
-          flexDirection: 'row',
-          backgroundColor: '#ffffff',
-          maxHeight: 40,
-          alignItems: 'center',
-          // borderLeftWidth: 0,
-          // borderRightWidth: 0,
-          borderBottom: 1,
-          borderColor: '#b5b3ca',
-          pl: 2
-        }}
-      >
-        <DashboardOutlinedIcon fontSize='medium' sx={{ color: '#262065' }} />
-        <Box sx={{ pl: 1, color: '#262065', display: 'flex', pt: 0.3 }} >My Dashboard</Box>
-      </Box>
-      <Box sx={{
-        display: 'flex',
-        flex: 1,
-        flexDirection: 'column',
-        flexWrap: 'wrap',
-        padding: 1,
-        backgroundColor: '#ffffff',
-        overflow: 'hidden'
-      }} >
+    return (
         <Box
-          sx={{ display: 'flex', paddingX: 3, paddingTop: 1 }}
-          gap={1}
-        >
-          {
-            data?.map((e, idx) => (
-              <DashBordTile
-                key={idx}
-                taskName={e.taskName}
-                count={e.count}
-                Icons={e.icon}
-              />
-            ))
-          }
+            sx={{
+                display: 'flex',
+                minHeight: `${innerHeight - 80}px`,
+                borderRadius: 2,
+                overflow: 'hidden',
+                flexDirection: 'column',
+                border: 1,
+                borderWidth: 1.5,
+                borderColor: taskColor.bgIndigo,
+            }} >
+            <Box
+                sx={{
+                    display: 'flex',
+                    flex: 1,
+                    flexDirection: 'row',
+                    backgroundColor: '#ffffff',
+                    maxHeight: 40,
+                    alignItems: 'center',
+                    borderBottom: 1,
+                    borderColor: '#b5b3ca',
+                    pl: 2
+                }}
+            >
+                <DashboardOutlinedIcon fontSize='medium' sx={{ color: '#262065' }} />
+                <Box sx={{ pl: 1, color: '#262065', display: 'flex', pt: 0.3 }} >My Dashboard</Box>
+            </Box>
+            <Box sx={{
+                display: 'flex',
+                flex: 1,
+                flexDirection: 'column',
+                flexWrap: 'wrap',
+                padding: 1,
+                backgroundColor: '#ffffff',
+                overflow: 'hidden'
+            }} >
+                <Box
+                    sx={{ display: 'flex', paddingX: 3, paddingTop: 1 }}
+                    gap={1}
+                >
+                    {
+                        data?.map((e, idx) => (
+                            <DashBordTile
+                                key={idx}
+                                taskName={e.taskName}
+                                count={e.count}
+                                Icons={e.icon}
+                            />
+                        ))
+                    }
 
+                </Box>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexGrow: 1,
+                        flexDirection: 'column',
+                    }}
+                >
+                    <MyTaskTable />
+                </Box>
+            </Box>
         </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            flexGrow: 1,
-            flexDirection: 'column',
-          }}
-        >
-          <MyTaskTable />
-        </Box>
-      </Box>
-    </Box>
-  )
+    )
 }
 
 export default memo(TaskHomePage) 
