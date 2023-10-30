84cb1e26692e5ece12f11642e39333028ab06d13
IT Medicity <itmedicity@tmc.ac.in>
2023-10-30 13:50:28 +0530

    dashboard Design

--- src/views/RoomManagement/DashBoard/DashBoardRoomSort.jsx
+++ src/views/RoomManagement/DashBoard/DashBoardRoomSort.jsx
@@ -1,8 +1,8 @@
-import React, { useCallback,useEffect, useState,memo } from 'react'
+import React, { useCallback, useEffect, useState, memo } from 'react'
 import { Box } from '@mui/material'
 import AccountTreeOutlinedIcon from '@mui/icons-material/AccountTreeOutlined'
 
-const DashBoardRoomSort = ({ blockno, data,setRoomNo, setRoomName,setAssetList,setBlockName}) => {
+const DashBoardRoomSort = ({ blockno, data, setRoomNo, setRoomName, setAssetList, setBlockName }) => {
   const [room, setRoom] = useState([])
   useEffect(() => {
     const all = data.filter((val) => {
@@ -10,15 +10,15 @@ const DashBoardRoomSort = ({ blockno, data,setRoomNo, setRoomName,setAssetList,s
     }, [])
     setRoom(all)
   }, [blockno, data])
-    const asset = useCallback(
-      (data) => {      
-      const { rm_room_slno, rm_room_name ,rm_insidebuildblock_name} = data
+  const asset = useCallback(
+    (data) => {
+      const { rm_room_slno, rm_room_name, rm_insidebuildblock_name } = data
       setRoomNo(rm_room_slno)
       setRoomName(rm_room_name)
-        setAssetList(1)
-        setBlockName(rm_insidebuildblock_name)      
+      setAssetList(1)
+      setBlockName(rm_insidebuildblock_name)
     },
-    [setRoomNo, setRoomName,setAssetList,setBlockName],
+    [setRoomNo, setRoomName, setAssetList, setBlockName],
   )
 
   return (
@@ -29,8 +29,10 @@ const DashBoardRoomSort = ({ blockno, data,setRoomNo, setRoomName,setAssetList,s
             key={index}
             sx={{
               display: 'flex',
-              height: 60,
-              width: { md: '12%', sm: '16%', xs: '16%', lg: '12%' },
+              margin: 'auto',
+              minHeight: 60,
+              maxHeight: 80,
+              width: { md: '30%', sm: '25%', xs: '16%', lg: '12%' },
               border: 0.5,
               backgroundColor: '#8486F3',
               m: 0.4,
@@ -43,10 +45,12 @@ const DashBoardRoomSort = ({ blockno, data,setRoomNo, setRoomName,setAssetList,s
             <Box
               sx={{
                 display: 'flex',
-                flex: 3,
+                flex: 2,
+                margin: 'auto',
                 fontSize: 12.5,
                 fontSmooth: 10,
                 fontWeight: 600,
+
               }}
             >
               {val.rm_room_name}
@@ -61,7 +65,7 @@ const DashBoardRoomSort = ({ blockno, data,setRoomNo, setRoomName,setAssetList,s
             >
               <AccountTreeOutlinedIcon sx={{ fontSize: 30 }}
                 onClick={() => asset(val)}
-                value={val.rm_room_name}/>
+                value={val.rm_room_name} />
             </Box>
           </Box>
         )
