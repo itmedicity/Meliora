0fe730102cde450dbb0eed47efb8828305e25ee0
unknown <Administrator@MEDICITY.CO.IN>
2024-01-13 12:47:34 +0530

    Avoid Login page whitening problem

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-
+const NotCorect = React.lazy(() => import('./views/dashboard/Home'))
 const Home = React.lazy(() => import('./views/dashboard/Home'))
 const Settings = React.lazy(() => import('../src/Menus/Settings'))
 const Administrtion = React.lazy(() => import('../src/views/Administration/Test'))
@@ -170,6 +170,7 @@ const DeptSecBasedItemReport = React.lazy(() => import('./views/Report/AssetRepo
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
+  { path: '/NotCorect', exact: true, name: 'NotCorect', component: NotCorect },
   { path: '/Home', exact: true, name: 'Home', component: Home },
   { path: '/Home/Settings', exact: true, name: 'Settings', component: Settings },
   { path: '/Home/Administration', exact: true, name: 'Administator', component: Administrtion },
