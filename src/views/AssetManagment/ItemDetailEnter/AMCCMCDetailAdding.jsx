18ed30bb139aa84b1b6d261e52ec3dad06a4b2a5
unknown <Administrator@MEDICITY.CO.IN>
2024-06-04 11:34:45 +0530

    Spare transfer to condemnation and service page done

--- src/views/AssetManagment/ItemDetailEnter/AMCCMCDetailAdding.jsx
+++ src/views/AssetManagment/ItemDetailEnter/AMCCMCDetailAdding.jsx
@@ -525,7 +525,7 @@ const AMCCMCDetailAdding = ({ detailArry, amcPm, setAmcPm, amcPmarry }) => {
                             </Box>
 
                             <Box sx={{ display: 'flex', width: '10%', p: 0.5, flexDirection: 'column' }} >
-                                <Typography sx={{ fontSize: 13, fontFamily: 'sans-serif', fontWeight: 550 }} >Enter Days</Typography>
+                                <Typography sx={{ fontSize: 13, fontFamily: 'sans-serif', fontWeight: 550 }} >Days</Typography>
                                 <Box>
                                     <TextFieldCustom
                                         type="number"
